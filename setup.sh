392f823029e5a1b43d8db5d78d2d29c1317d1dd6
Jason Lu <jasonbangbang@gmail.com>
2016-04-23 16:13:04 -0500

    Automatically install Arduino and Arduino-core

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -fy timidity libasound2-dev
+sudo apt-get install -fy timidity libasound2-dev arduino arduino-core
 wget http://www.varal.org/ttymidi/ttymidi.tar.gz
 tar xvf ttymidi.tar.gz
 cd ttymidi
