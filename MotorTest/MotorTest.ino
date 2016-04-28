190d5c0578a5f36c7dc083008387dfdaf87cfa17
Jason Lu <jasonbangbang@gmail.com>
2016-04-28 10:48:01 -0500

    Add actual motor code

---
+++ MotorTest/MotorTest.ino
@@ -0,0 +1,125 @@
+/*-----( Import needed libraries )-----*/
+#include <Stepper.h>
+
+/*-----( Declare Constants, Pin Numbers )-----*/
+//---( Number of steps per revolution of INTERNAL motor in 4-step mode )---
+#define STEPS_PER_MOTOR_REVOLUTION 32   
+
+//---( Steps per OUTPUT SHAFT of gear reduction )---
+#define STEPS_PER_OUTPUT_REVOLUTION 128  // 5  
+
+#define lightPin 0
+#define laserPin 7
+#define motorDelay 3
+#define laserDelay 5
+#define threshold 70
+#define motorSpeed 700
+
+int note9 = 0x61;
+int note8 = 0x63;
+int note7 = 0x64;
+int note6 = 0x66;
+int note5 = 0x68;
+int note4 = 0x70;
+int note3 = 0x71;
+int note2 = 0x40;
+int note1 = 0x47;
+
+Stepper small_stepper(STEPS_PER_MOTOR_REVOLUTION, 8, 10, 9, 11);
+
+int  Steps2Take;
+int current;
+int stepID;
+int light;
+
+void setup()   /*----( SETUP: RUNS ONCE )----*/
+{
+// Nothing  (Stepper Library sets pins as outputs)
+  Serial.begin(115200);
+  pinMode(laserPin, OUTPUT);
+}/*--(end setup )---*/
+
+void parsePosition() {
+  switch (stepID) {
+    case 1:
+      noteOn(0x90, note1, 0x7F);
+      break;
+    case 2:
+      noteOn(0x90, note2, 0x7F);
+      break;
+    case 3:
+      noteOn(0x90, note3, 0x7F);
+      break;
+    case 4:
+      noteOn(0x90, note4, 0x7F);
+      break;
+    case 5:
+      noteOn(0x90, note5, 0x7F);
+      break;
+    case 6:
+      noteOn(0x90, note6, 0x7F);
+      break;
+    case 7:
+      noteOn(0x90, note7, 0x7F);
+      break;
+    case 8:
+      noteOn(0x90, note8, 0x7F);
+      break;
+    default:
+      noteOn(0x90, note1, 0x00);
+      break;
+  }
+}       
+
+void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
+{
+  for (int i = 1; i <= 8; i++) {
+    digitalWrite(laserPin, LOW);
+    small_stepper.setSpeed(motorSpeed);
+    small_stepper.step(16);
+    stepID = i;
+    delay(motorDelay);
+    digitalWrite(laserPin, HIGH);
+    delay(laserDelay);
+    light = analogRead(lightPin);
+    if (light > threshold) {
+      parsePosition();
+    } else {
+      noteOn(0x90, 0x1E, 0x00);
+    }
+//    Serial.print("ID: ");
+//    Serial.print(stepID);
+//    Serial.print(" Value: ");
+//    Serial.println(light);
+  }
+  // delay(1000);
+  for (int i = 8; i >= 1; i--) {
+    digitalWrite(laserPin, LOW);
+    small_stepper.setSpeed(motorSpeed);
+    small_stepper.step(-16);
+    stepID = i;
+    delay(motorDelay);
+    digitalWrite(laserPin, HIGH);
+    delay(laserDelay);
+    light = analogRead(lightPin);
+    if (light > threshold) {
+      parsePosition();
+    } else {
+      noteOn(0x90, 0x1E, 0x00);
+    }
+//    Serial.print("ID: ");
+//    Serial.print(stepID);
+//    Serial.print(" Value: ");
+//    Serial.println(light);
+  }
+  // delay(2000);
+
+}/* --(end main loop )-- */
+
+void noteOn(int cmd, int pitch, int velocity) {
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(velocity);
+}
+
+/* ( THE END ) */
