14bdc8510426fa06bfbe9dd548d83b6c2d706097
Jason Lu <jasonbangbang@gmail.com>
2016-05-03 11:42:39 -0500

    Add note set switching capabilities

--- Guzheng/Guzheng.ino
+++ Guzheng/Guzheng.ino
@@ -1,11 +1,10 @@
 /* Code for laser guzheng
  * Copyright (C) 2016 Jason Lu
  * 
- */
- 
- 
+ */ 
+
 // Some defines
-#define THRESHOLD 700
+#define THRESHOLD 500
 #define VOLUME 0x7F
 #define LASTPORT 4
 #define DELAY 10
@@ -16,7 +15,7 @@
 #define CHANGEINSTRUMENT 0xC0
 
 // Values for different notes
-int note8 = 0x68;
+int note8 = 0x48;
 int note7 = 0x47;
 int note6 = 0x45;
 int note5 = 0x43;
@@ -24,6 +23,8 @@ int note4 = 0x41;
 int note3 = 0x40;
 int note2 = 0x3E;
 int note1 = 0x3C;
+
+int noteSet = 1;
 
 void setup()
 {
@@ -35,73 +36,77 @@ void setup()
   // 0xC0 is the change instrument command
   // 0x39 is the instrument ID in hex
   Serial.write(CHANGEINSTRUMENT);
-  Serial.write(0x39);
+  Serial.write(0x68);
 }
 
 // Function for playing note based on ID
 void musicOn(int ID) {
-  switch (ID) {
-  case 1:
-    noteOn(NOTEON, note1, VOLUME);
-    break;
-  case 2:
-    noteOn(NOTEON, note2, VOLUME);
-    break;
-  case 3:
-    noteOn(NOTEON, note3, VOLUME);
-    break;
-  case 4:
-    noteOn(NOTEON, note4, VOLUME);
-    break;
-  case 5:
-    noteOn(NOTEON, note5, VOLUME);
-    break;
-  case 6:
-    noteOn(NOTEON, note6, VOLUME);
-    break;
-  case 7:
-    noteOn(NOTEON, note7, VOLUME);
-    break;
-  case 8:
-    noteOn(NOTEON, note8, VOLUME);
-    break;
-  default:
-    noteOn(NOTEOFF, note1, 0x00);
-    break;
+  if (noteSet == 1) {
+    switch (ID) {
+      case 1:
+        noteOn(NOTEON, note1, VOLUME);
+        break;
+      case 2:
+        noteOn(NOTEON, note2, VOLUME);
+        break;
+      case 3:
+        noteOn(NOTEON, note3, VOLUME);
+        break;
+      case 4:
+        noteOn(NOTEON, note4, VOLUME);
+        break;
+    }
+  } else if (noteSet == 2) {
+    switch (ID) {
+      case 1:
+        noteOn(NOTEON, note5, VOLUME);
+        break;
+      case 2:
+        noteOn(NOTEON, note6, VOLUME);
+        break;
+      case 3:
+        noteOn(NOTEON, note7, VOLUME);
+        break;
+      case 4:
+        noteOn(NOTEON, note8, VOLUME);
+        break;
+    }
   }
 }
 
 // Function for turning off a specific note based on ID
 // Just a slightly modified version of musicOn()
 void musicOff(int ID) {
-  switch (ID) {
-  case 1:
-    noteOn(NOTEOFF, note1, 0x7F);
-    break;
-  case 2:
-    noteOn(NOTEOFF, note2, 0x7F);
-    break;
-  case 3:
-    noteOn(NOTEOFF, note3, 0x7F);
-    break;
-  case 4:
-    noteOn(NOTEOFF, note4, 0x7F);
-    break;
-  case 5:
-    noteOn(NOTEOFF, note5, 0x7F);
-    break;
-  case 6:
-    noteOn(NOTEOFF, note6, 0x7F);
-    break;
-  case 7:
-    noteOn(NOTEOFF, note7, 0x7F);
-    break;
-  case 8:
-    noteOn(NOTEOFF, note8, 0x7F);
-    break;
-  default:
-    noteOn(NOTEOFF, note1, 0x00);
-    break;
+  if (noteSet == 1) {
+    switch (ID) {
+      case 1:
+        noteOn(NOTEOFF, note1, VOLUME);
+        break;
+      case 2:
+        noteOn(NOTEOFF, note2, VOLUME);
+        break;
+      case 3:
+        noteOn(NOTEOFF, note3, VOLUME);
+        break;
+      case 4:
+        noteOn(NOTEOFF, note4, VOLUME);
+        break;
+    }
+  } else if (noteSet == 2) {
+    switch (ID) {
+      case 1:
+        noteOn(NOTEOFF, note5, VOLUME);
+        break;
+      case 2:
+        noteOn(NOTEOFF, note6, VOLUME);
+        break;
+      case 3:
+        noteOn(NOTEOFF, note7, VOLUME);
+        break;
+      case 4:
+        noteOn(NOTEOFF, note8, VOLUME);
+        break;
+    }
   }
 }
 
@@ -113,7 +118,15 @@ void loop()
     if(analogRead(i) > THRESHOLD) {
       // Beam has been cut
       // i+1 is because i is 0 indexed, but the ID starts with 1
-      musicOn(i + 1);
+      if (i < 4) {
+        musicOn(i + 1);
+      } else {
+        if (noteSet == 1) {
+          noteSet = 2;
+        } else {
+          noteSet = 1;
+        }
+      }
     } else {
       // Hand is gone. Stop the sound
       musicOff(i + 1);
