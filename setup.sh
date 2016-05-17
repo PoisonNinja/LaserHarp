a478e589c97d3111d96ff7b7de407c97aa43efcb
Jason Lu <jasonbangbang@gmail.com>
2016-05-17 16:52:32 -0500

    setup: Exit on error

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install -fy timidity libasound2-dev arduino arduino-core
-wget http://www.varal.org/ttymidi/ttymidi.tar.gz
-tar xvf ttymidi.tar.gz
-cd ttymidi
-patch < ../ttymidi.patch
-make
-sudo cp ttymidi /usr/local/bin
-cd ../
+function bail() {
+    echo "An error occurred. Please check the messages to see what went wrong"
+    exit 1
+}
+
+sudo apt-get install -fy timidity libasound2-dev arduino arduino-core || bail
+wget http://www.varal.org/ttymidi/ttymidi.tar.gz || bail
+tar xvf ttymidi.tar.gz || bail
+cd ttymidi || bail
+patch < ../ttymidi.patch || bail
+make || bail
+sudo cp ttymidi /usr/local/bin || bail
+cd ../ 
 rm ttymidi/ -rf ttymidi.tar.gz
