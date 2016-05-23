9f049c6b2318a1603088b1e83de969b9df4b03ca
Jason Lu <jasonbangbang@gmail.com>
2016-05-23 08:56:51 -0500

    Revert "Remove harp code"
    This reverts commit fc093578bbda0462772f6322f3f2ab65b20340cf.

---
+++ Harp/server.h
@@ -0,0 +1,13 @@
+#define note1 0x3C
+#define note2 0x3E
+#define note3 0x40
+#define note4 0x41
+#define note5 0x43
+#define note6 0x45
+#define note7 0x47
+#define note8 0x48
+
+#define VOLUME 0x7F
+
+#define NOTEON 0x90
+#define NOTEOFF 0x80
